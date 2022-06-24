ecfad610d1f53cd762f07ff92b6a54125d6f4b3b
Andrew Popelnukha <popelnucha70@gmail.com>
2022-06-24 15:55:18 +0300

    add failed

--- database/migrations/2022_06_24_121136_create_patient_table.php
+++ database/migrations/2022_06_24_121136_create_patient_table.php
@@ -16,6 +16,17 @@ class CreatePatientTable extends Migration
         Schema::create('patient', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('question');
+            $table->string('question_1');
+            $table->string('question_2');
+            $table->string('question_3');
+            $table->string('question_4');
+            $table->string('question_5');
+            $table->string('question_6');
+            $table->string('question_7');
+            $table->string('question_8');
+            $table->string('question_9');
+
         });
     }
 
