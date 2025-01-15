0da6360952928916d5ac2d6f83db75f0bd9e66e3
dn050694pah <andrii.popelnukha@privatbank.ua>
2025-01-15 15:26:08 +0200

    fix version 2.0

--- resources/views/vis.blade.php
+++ resources/views/vis.blade.php
@@ -124,7 +124,7 @@
     canvas.height = mmToPx(canvasHeightMM);
 
     let image = new Image();
-    image.src = '{{ asset('storage/' . $imagePath) }}';
+    image.src = '{{ asset('storage/app/public/vision' . $imagePath) }}';
 
     let drawing = false;
     let startX = -1, startY = -1;
