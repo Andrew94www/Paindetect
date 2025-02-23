dd741221537c3eaf919a43f6be4ed6962a2686ba
dn050694pah <andrii.popelnukha@privatbank.ua>
2025-02-23 22:23:51 +0200

    fix version 10

---
+++ app/Models/PainRecord.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+
+class PainRecord extends Model
+{
+    use HasFactory;
+    protected $fillable = ['image', 'pain_level', 'medications'];
+
+    public function getImageUrlAttribute()
+    {
+        return asset('storage/' . $this->image);
+    }
+
+}
